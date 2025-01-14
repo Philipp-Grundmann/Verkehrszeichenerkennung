12b7a0a289f5edb43e9870dbbfd50be993149e0f
Philipp-Grundmann <philipp_grundmann@t-online.de>
2025-01-14 13:07:19 +0100

    erste Versuche mit smile der funktioniert

--- src/de/hszg/learner/k_means_cluster/test.java
+++ src/de/hszg/learner/k_means_cluster/test.java
@@ -1,55 +1,42 @@
+package de.hszg.learner.k_means_cluster;
+
 import smile.clustering.KMeans;
-import smile.plot.swing.ScatterPlot;
-import smile.plot.swing.Canvas;
-import java.awt.Color;
+import java.util.Arrays;
+
 public class test {
 
     public static void main(String[] args) {
-        // Beispiel-Datensatz
-        double[][] data = {
-                {1.0, 2.0},
-                {1.5, 1.8},
-                {5.0, 8.0},
-                {8.0, 8.0},
-                {1.0, 0.6},
-                {9.0, 11.0},
-                {8.0, 2.0},
-                {10.0, 2.0},
-                {9.0, 3.0}
-        };
 
-        // Anzahl der Cluster
-        int k = 2;
 
-        // k-Means ausführen
-        KMeans kmeans = KMeans.fit(data, k);
+                // Gegebenes Set von Merkmalsvektoren (z. B. 2D-Vektoren)
+                double[][] featureVectors = {
+                        {1.0, 2.0},
+                        {1.5, 1.8},
+                        {5.0, 8.0},
+                        {8.0, 8.0},
+                        {1.0, 0.6},
+                        {9.0, 11.0},
+                        {8.0, 2.0},
+                        {10.0, 2.0},
+                        {9.0, 3.0}
+                };
 
-        // Ergebnisse anzeigen
-        System.out.println("Cluster-Zentren:");
-        for (double[] center : kmeans.centroids) {
-            System.out.printf("Center: [%.2f, %.2f]%n", center[0], center[1]);
-        }
+                // Anzahl der Cluster, die gefunden werden sollen
+                int k = 6;
 
-        System.out.println("\nCluster-Zuweisungen:");
-        for (int i = 0; i < data.length; i++) {
-            System.out.printf("Punkt [%.2f, %.2f] -> Cluster %d%n", data[i][0], data[i][1], kmeans.y[i]);
-        }
+                // k-Means ausführen
+                KMeans kmeans = KMeans.fit(featureVectors, k);
 
-        // Visualisierung vorbereiten
-        Color[] colors = {Color.RED, Color.BLUE, Color.GREEN, Color.ORANGE, Color.MAGENTA}; // Mehr Farben hinzufügen, wenn nötig
-        Color[] pointColors = new Color[data.length];
-        for (int i = 0; i < data.length; i++) {
-            pointColors[i] = colors[kmeans.y[i] % colors.length];
-        }
+                // Ergebnisse anzeigen
+                System.out.println("Cluster-Zentren:");
+                Arrays.stream(kmeans.centroids).forEach(center ->
+                        System.out.printf("Center: [%.2f, %.2f]%n", center[0], center[1])
+                );
 
-        // Streudiagramm zeichnen
-        ScatterPlot plot = ScatterPlot.of(data, pointColors);
-
-        // Canvas anzeigen
-        Canvas canvas = new Canvas(plot);
-        canvas.setTitle("K-Means Clustering");
-        canvas.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        canvas.show();
+                System.out.println("\nCluster-Zuweisungen:");
+                for (int i = 0; i < featureVectors.length; i++) {
+                    System.out.printf("Vektor [%.2f, %.2f] -> Cluster %d%n", featureVectors[i][0], featureVectors[i][1], kmeans.y[i]);
+                }
     }
 }
 
