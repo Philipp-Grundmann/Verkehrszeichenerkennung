c0fe5ec68dab3f76653b34fe7136d63b348a31d5
Vincent Montag <vinimo2708@gmail.com>
2025-01-24 11:31:39 +0100

    path fix

--- src/de/hszg/learner/K_X_Means/K_Means.java
+++ src/de/hszg/learner/K_X_Means/K_Means.java
@@ -2,15 +2,25 @@ package de.hszg.learner.K_X_Means;
 
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class K_Means {
 
     public static void main(String[] args) {
         try {
-            String csvFilePath = "D:\\1.2_Master\\IdeaProjects\\ML-3\\src\\de\\hszg\\learner\\ergebnisse\\VektorData_20250121_193812_S7xZ7.csv";// Pfad zu den FV
-            String outputPath = "D:\\1.2_Master\\IdeaProjects\\ML-3\\src\\de\\hszg\\learner\\ergebnisse\\kmeans_results.csv";// Pfad zur Ausgabe-CSV-Datei der Cluster
+            //String csvFilePath = "D:\\1.2_Master\\IdeaProjects\\ML-3\\src\\de\\hszg\\learner\\ergebnisse\\VektorData_20250121_193812_S7xZ7.csv";// Pfad zu den FV
+            //String outputPath = "D:\\1.2_Master\\IdeaProjects\\ML-3\\src\\de\\hszg\\learner\\ergebnisse\\kmeans_results.csv";// Pfad zur Ausgabe-CSV-Datei der Cluster
             int randomSeed = 42; //Seed
             double trainSplitRatio = 0.8;   // einstellung wie viele der gesamtdaten zu trainingsdaten werden
+
+            String outputPath = "src/de/hszg/learner/ergebnisse/kmeans_results.csv";
+            String csvFilePath = "src/de/hszg/learner/ergebnisse/VektorData_20250121_193812_S7xZ7.csv";
+
+
+            //String csvData = new String(Files.readAllBytes(Paths.get(outputPath)));
+            // Ausgabe des Inhalts
+            //System.out.println(csvData);
 
             DataLoader dataLoader = new DataLoader(csvFilePath, randomSeed);
             DataLoader.DataSet dataSet = dataLoader.loadData(trainSplitRatio);
